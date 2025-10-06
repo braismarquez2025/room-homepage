e3656f103013cbeabdf8b6e9e2dc8a5737f7b37f
Brais <braismarquez03@gmail.com>
2025-10-06 12:50:13 +0200

    update

--- src/context/ModelsContext.jsx
+++ src/context/ModelsContext.jsx
@@ -22,13 +22,13 @@ export const ModelsProvider = ({ children }) => {
         {
           title: "We are available all across the globe",
           description: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today. ",
-          image: heroMobile2,
+          imageMobile: heroMobile2,
           imageDesktop: heroDesktop2
         },
         {
           title: "Manufactured with the best materials",
           description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
-          image: heroMobile3,
+          imageMobile: heroMobile3,
           imageDesktop: heroDesktop3
         },
       ];
