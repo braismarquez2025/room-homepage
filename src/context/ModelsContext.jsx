9a439c4a21ff5dac71fa1bc479785654464fa490
Brais <braismarquez03@gmail.com>
2025-10-06 12:42:24 +0200

    update

--- src/context/ModelsContext.jsx
+++ src/context/ModelsContext.jsx
@@ -1,6 +1,13 @@
 import { createContext } from "react";
 import { useState } from "react";
 
+import heroMobile1 from '@/assets/images/mobile-image-hero-1.jpg';
+import heroDesktop1 from '@/assets/images/desktop-image-hero-1.jpg';
+import heroMobile2 from '@/assets/images/mobile-image-hero-2.jpg';
+import heroDesktop2 from '@/assets/images/desktop-image-hero-2.jpg';
+import heroMobile3 from '@/assets/images/mobile-image-hero-3.jpg';
+import heroDesktop3 from '@/assets/images/desktop-image-hero-3.jpg';
+
 // eslint-disable-next-line react-refresh/only-export-components
 export const ModelsContext = createContext();
 
@@ -9,20 +16,20 @@ export const ModelsProvider = ({ children }) => {
         {
           title: "Discover innovative ways to decorate",
           description: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
-          imageMobile: "assets/images/mobile-image-hero-1.jpg",
-          imageDesktop: "assets/images/desktop-image-hero-1.jpg",
+          imageMobile: heroMobile1,
+          imageDesktop: heroDesktop1
         },
         {
           title: "We are available all across the globe",
           description: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today. ",
-          image: "src/assets/images/mobile-image-hero-2.jpg",
-          imageDesktop: "src/assets/images/desktop-image-hero-2.jpg",
+          image: heroMobile2,
+          imageDesktop: heroDesktop2
         },
         {
           title: "Manufactured with the best materials",
           description: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
-          image: "src/assets/images/mobile-image-hero-3.jpg",
-          imageDesktop: "src/assets/images/desktop-image-hero-3.jpg",
+          image: heroMobile3,
+          imageDesktop: heroDesktop3
         },
       ];
 
